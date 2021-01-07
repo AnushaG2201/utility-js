202a40d5f729bd1094207257b7a3aeb9639a2673
Anusha <anusha.g@thoughtworks.com>
2021-01-07 19:20:16 +0530

    Updating changes for the failed test to pass

--- src/Assignment/test.js
+++ src/Assignment/test.js
@@ -2,7 +2,7 @@ const operations = require('./node_operations');
 
 describe ('Return Head Value', () => {
     it('Head Value of [1,2,3] is 1', () => {
-        expect(head([1,2,3])).toEqual(1);
+        expect(operations([1,2,3])).toEqual(1);
     });
-}
+})
 
--- src/add.js
+++
@@ -1,9 +0,0 @@
-const add = (first,second) => {
-    return first + second;   
-}
-
-const head = (arr) => {
-    return arr[0];
-}
-module.exports = {add,head};
-//module.exports = head;
