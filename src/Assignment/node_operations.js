f03d608206c675a22248ee469f792ead8a51899f
Anusha <anusha.g@thoughtworks.com>
2021-01-09 12:53:17 +0530

    Add filter functionality and min max functionality

--- src/Assignment/node_operations.js
+++ src/Assignment/node_operations.js
@@ -12,20 +12,36 @@ const tail = (arr) => {
  }
 
  const map = (arr, cube) => {
-
     if(arr.length != 0){
         var numToBeCubed=arr.shift();
         var cubedValue = cube(numToBeCubed);
         array.push(cubedValue);
         console.log(array.length);
         map(arr,cube);
-        return array;
         }
-        return [];
+        return array;
 }
 var storeCube = (arrVariable) => {
     var result = arrVariable*arrVariable*arrVariable;
     return result;
     }
 
-module.exports = {head:head, tail:tail, map:map, storeCube:storeCube};
+const filter = (arr,x) => {
+    if(x == true)
+    return arr;
+    else{
+    arr = [];
+    return arr;
+    }
+    }
+
+const max = (arr) => {
+       //var maxValue = arr.reduce(function(value1,value2) {
+       return Math.max(...arr);
+      // });
+       }
+const min = (arr) => {
+       return Math.min(...arr);
+       }
+
+module.exports = {head:head, tail:tail, map:map, storeCube:storeCube, filter:filter, max:max, min:min};
--- src/Assignment/test.js
+++ src/Assignment/test.js
@@ -1,9 +1,9 @@
 const operations = require('./node_operations');
-
 describe ('Return Head Value', () => {
     it('Head Value of [] is undefined', () => {
         expect(operations.head([])).toEqual(undefined);
     });
+
     it('Head Value of [1,2,3] is 1', () => {
         expect(operations.head([1,2,3])).toEqual(1);
     });
@@ -13,19 +13,48 @@ describe ('Return Tail value' , () => {
     it('Tail value of [1,2,3] is [2,3]' , () => {
         expect(operations.tail([1,2,3])).toEqual([2,3]);
         });
+
     it('Tail value of [] is []' , () => {
         expect(operations.tail([])).toEqual([]);
         });
 })
 
 describe ('Return Mapped Operation' , () => {
-    afterEach(() => {
-    sandbox..restore();
-    })
+
+    it('Mapped value of [] is []' , () => {
+       expect(operations.map([],operations.storeCube)).toEqual([])
+       });
+
     it('Mapped value of ([2,3,4],cube) is [8,27,64]', () =>{
         expect(operations.map([2, 3, 4],operations.storeCube)).toEqual([8, 27, 64]);
         });
-    it('Mapped value of [] is []' , () => {
-        expect(operations.map([],operations.storeCube)).toEqual([])
-        });
+
      })
+
+describe ('Return Value according to filter', () => {
+
+    it('Filter of [],true is []', () =>{
+    expect(operations.filter([],true)).toEqual([]);
+       });
+
+    it('Filter of [1, 2, 3],true is [1, 2, 3]', () =>{
+        expect(operations.filter([1, 2, 3],true)).toEqual([1, 2, 3]);
+        });
+
+    it('Filter of [1, 2, 3],false is []', () =>{
+        expect(operations.filter([1, 2, 3], false)).toEqual([])
+        });
+
+    })
+
+describe('Return the maximum Value in an array', () => {
+    it('Maximum of [1,2,3,4] is 4', () => {
+        expect(operations.max([1,2,3,4])).toEqual(4)
+        });
+        })
+
+describe('Return the minimum value in an array', () => {
+    it('The minimum of [1,2,3,4] is 1', () => {
+        expect(operations.min([1,2,3,4])).toEqual(1)
+        });
+        })
