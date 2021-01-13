6cf09698317a79153269990d1f7a9686952188bb
Anusha G <anusha.g@thoughtworks.com>
2021-01-13 08:32:51 +0530

    Update node_operations

--- src/Assignment/node_operations.js
+++ src/Assignment/node_operations.js
@@ -12,45 +12,98 @@ const tail = (arr) => {
  }
 
 const map = (arr,mapOperation) => {
-             var mapResult;
-             mapResult = arr.map(mapOperation);
-             return mapResult;
- }
-var returnCube = (num) => Math.pow(num,3);
-var returnIdentity = (num) => num * 1;
-var returnObject = (obj) => obj.x+1;
-
-const filter = (arr, filterOperation) => {
-      if(arr.length == 0)
-        return arr;
-       var result;
-       result = arr.filter(filterOperation);
-       return result;
+ var mapResult = [];
+ mapFunctions(mapResult,arr,mapOperation);
+ return mapResult;
 }
 
-var filterUpperCase = (char) => char > 'A' && char < 'Z';
+var mapFunctions = (mapResult, arr, mapOperation) => {
+    if(arr.length != 0){
+    var num  = arr.shift();
+    mapResult.push(mapOperation(num));
+    mapFunctions(mapResult,arr,mapOperation);
+    }
+}
+
+var returnCube = (num) => Math.pow(num, 3);
+var returnIdentity = (num) => num*1;
+var returnObject = (num) => (num.x)+1;
+
+
+const filter = (arr, filterOperation) => {
+       var filterResult = [];
+       filterFunctions(filterResult, arr, filterOperation);
+       return filterResult;
+}
+
+var filterFunctions = (filterResult, arr, filterOperation) => {
+      if(arr.length != 0){
+           var num = arr.shift();
+           var result = filterOperation(num);
+           if(result)
+                filterResult.push(num);
+           filterFunctions(filterResult, arr, filterOperation)
+           }
+}
+
+var filterUpperCase = (char) => char > 'A' && char < 'Z'
 var returnTrue = (num) => num;
 var returnFalse = (num) => !num;
 var filterValuesGreaterThanX = (num) => num > 1;
 
+
 const reduce = (arr,reducerFunc,additionalValue) => {
-      if(arr.length == 0)
-         return arr;
-      if(additionalValue == undefined)
-         result = arr.reduce(reducerFunc);
-      else{
-          result = arr.reduce(reducerFunc,additionalValue);
-        }
-      return result;
+    var reduceResult;
+    if(arr.length == 0)
+        return arr;
+    if(!additionalValue){
+        if(typeof(arr[0]) == "number")
+            reduceResult = 0;
+        else if(typeof(arr[0]) == "string")
+            reduceResult="";
+      }
+    else
+        reduceResult = additionalValue;
+    console.log(reduceResult);
+    reduceResult = reducer(reduceResult, arr, reducerFunc);
+    return reduceResult;
  }
+var reducer = (reduceResult, arr, reducerFunc) => {
+    if(arr.length != 0) {
+        var num = arr.shift();
+    reduceResult = reducerFunc(reduceResult,num);
+    return reducer(reduceResult, arr, reducerFunc);
+  }
+    return reduceResult;
+}
 var returnReduce = (x,y) => x+y;
 
 const max = (arr) => {
-       return Math.max(...arr);
+    var max = 0;
+    var result = maxFinder(arr, max);
+    return result;
+}
+const maxFinder = (arr, max) => {
+    if(arr.length != 0)
+    {
+    var num = arr.shift();
+    max = maxFinder(arr, Math.max(num, max));
+    }
+return max;
 }
 
 const min = (arr) => {
-       return Math.min(...arr);
+    var min = 9999;
+    var result = minFinder(arr, min);
+    return result;
+}
+const minFinder = (arr, min) => {
+    if(arr.length != 0)
+    {
+    var num = arr.shift();
+    min = minFinder(arr, Math.min(num, min));
+    }
+return min;
 }
 
 
@@ -68,6 +121,8 @@ returnFalse:returnFalse,
 returnTrue:returnTrue,
 max:max,
 min:min,
+maxFinder:maxFinder,
+minFinder:minFinder,
 returnReduce:returnReduce,
 reduce:reduce,
 };
